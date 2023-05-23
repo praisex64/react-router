7e242c98bddbf71784c2b5f8237469d951e80c16
praisex64 <akpevweess1@gmail.com>
2023-05-23 09:15:04 +0100

    feat: added the home and the about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <App />
+  ,
 )
