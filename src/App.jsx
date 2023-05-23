6b9d4f15edefc6b6dd13eb5c50f4d2c2f6e729d0
praisex64 <akpevweess1@gmail.com>
2023-05-23 10:29:07 +0100

    feat:add the vans page to the file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import About from './pages/About'; 
 import Home from './pages/Home'
+import Vans from './pages/Vans'
 
 
 function App() {
@@ -13,9 +14,11 @@ function App() {
      <BrowserRouter>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      <Link to="/vans">Vans</Link>
     <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} /> 
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+ 
+
+export default function Vans() {
+    return (
+        <div>
+            <h1>This is The vans page</h1>
+        </div>
+    )
+}
